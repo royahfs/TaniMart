432fb306204d745bca5ee07daaf5ab63711a8339
fatiya <fatiyaayumi190405@gmail.com>
2025-09-29 00:35:01 +0700

    transaksi

--- app/src/main/java/com/example/tanimart/ui/kasir/transaksi/TransaksiActivity.java
+++ app/src/main/java/com/example/tanimart/ui/kasir/transaksi/TransaksiActivity.java
@@ -1,10 +1,14 @@
 package com.example.tanimart.ui.kasir.transaksi;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tanimart.R;
 import com.example.tanimart.data.model.Product;
+import com.example.tanimart.databinding.ActivityTransaksiBinding;
 import com.example.tanimart.ui.adapter.ProductAdapter;
 import com.example.tanimart.ui.adapter.TagihanAdapter;
 
@@ -28,6 +33,7 @@ public class TransaksiActivity extends AppCompatActivity {
     private ProductAdapter productAdapter;
     private TransaksiViewModel transaksiViewModel;
     private EditText searchProduk;
+    ActivityTransaksiBinding binding;
 
     private List<Product> semuaProduk = new ArrayList<>();
 
@@ -35,6 +41,7 @@ public class TransaksiActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi);
+        binding = ActivityTransaksiBinding.inflate(getLayoutInflater());
 
         // drawer layout
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout_transaksi);
@@ -81,6 +88,27 @@ public class TransaksiActivity extends AppCompatActivity {
             productAdapter.setProductList(produkList);
         });
 
+
+        //bottom sheets
+        binding.bottomSheetConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+            }
+        });
+
+    }
+
+    private void showBottomSheet() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.bottom_sheet_layout);
+
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+        RecyclerView recyclerView= dialog.findViewById(R.id.recyclerCart);
+        TextView totalItem = dialog.findViewById(R.id.totalItem);
+        TextView angkaTotal = dialog.findViewById(R.id.angkaTotal);
+        Button btnSimpan = dialog.findViewById(R.id.btnSimpan);
+        Button btnBayar = dialog.findViewById(R.id.btnBayar);
     }
 
     private void filterProduk(String keyword) {
